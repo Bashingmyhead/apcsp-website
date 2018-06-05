8d3673629a60f176ff9e3a294ee4d3fd2620616c
Bashingmyhead <amyan726@gmail.com>
2018-06-05 22:16:35 +0000

    finally done with the linux site

---
+++ myproject/mainStory.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "stage1.h"
+
+int main()
+{
+	stage1();
+}
---
+++ myproject/stage2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void stage2()
+{
+	char input[256]
+
+	printf("STAGE TWO: Toddlerhood\n");
+	printf("Make a choice:\n");
+	if (
+}
